17ec5c402f21320a46235a781979a1ad96f79b4f
Suvesh Moza <hellosuvesh@gmail.com>
2026-05-29 23:09:50 +0530

    feat: add JSON config and measure avg rtt per DNS

--- main.go
+++ main.go
@@ -2,37 +2,84 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"time"
 
 	"codeberg.org/miekg/dns"
 )
 
+type ConfigFile struct {
+	Config     AppConfig `json:"config"`
+	DNSServers []Server  `json:"servers"`
+}
+
+type AppConfig struct {
+	Samples     int      `json:"samples"`
+	TimeoutMs   int      `json:"timeout_ms"`
+	TestDomains []string `json:"test_domains"`
+}
+
+type Server struct {
+	Name    string `json:"name"`
+	Address string `json:"address"`
+	Port    int    `json:"port"`
+}
+
+func loadConfig(path string) (ConfigFile, error) {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return ConfigFile{}, fmt.Errorf("read config: %w", err)
+	}
+
+	var cfg ConfigFile
+	if err := json.Unmarshal(content, &cfg); err != nil {
+		return ConfigFile{}, fmt.Errorf("parse config: %w", err)
+	}
+
+	return cfg, nil
+}
+
 func main() {
 
-	client := dns.NewClient()
-	results := make([]time.Duration, 0)
+	cfg, err := loadConfig("config.json")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-	for range 10 {
-		msg := dns.NewMsg("google.com", dns.TypeA)
-		addr := net.JoinHostPort("1.1.1.1", "53")
+	leaderboard := make(map[string]time.Duration)
 
-		_, rtt, err := client.Exchange(context.Background(), msg, "udp", addr)
-		if err != nil {
-			fmt.Printf("Error exchanging: %v\n", err)
-			os.Exit(1)
+	for _, server := range cfg.DNSServers {
+
+		client := dns.NewClient()
+		results := make([]time.Duration, 0)
+
+		for range 10 {
+			msg := dns.NewMsg("google.com", dns.TypeA)
+			addr := net.JoinHostPort(server.Address, strconv.Itoa(server.Port))
+
+			_, rtt, err := client.Exchange(context.Background(), msg, "udp", addr)
+			if err != nil {
+				fmt.Printf("Error exchanging: %v\n", err)
+				os.Exit(1)
+			}
+
+			results = append(results, rtt)
 		}
 
-		results = append(results, rtt)
+		var avg time.Duration
+		for _, result := range results {
+			avg += result
+		}
+		avg /= time.Duration(len(results))
+		leaderboard[server.Name] = avg
+
 	}
 
-	var average time.Duration
-	for _, result := range results {
-		average += result
-	}
-	fmt.Println(average)
-	average /= time.Duration(len(results))
-	fmt.Printf("Average RTT: %v\n", average)
+	fmt.Println(leaderboard)
+
 }
